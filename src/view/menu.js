4795bd731d3a5444093b4f0da2f092734d53871b
shvor <shvornev.dmitry@yandex.ru>
2021-08-04 18:07:54 +0300

    Добавляет взаимодействие с моковыми данными

--- src/view/menu.js
+++ src/view/menu.js
@@ -1,9 +1,16 @@
-export const createMenuTemplate = () => (`<nav class="main-navigation">
+export const cardsToFilterMap = {
+  all: (cards) => cards.length,
+  watchlist: (cards) => cards.filter((card) => card.isInWatchList).length,
+  history: (cards) => cards.filter((card) => card.isAlreadyWatched).length,
+  favorites: (cards) => cards.filter((card) => card.isInFavorites).length,
+};
+
+export const createMenuTemplate = (cards) => (`<nav class="main-navigation">
   <div class="main-navigation__items">
     <a href="#all" class="main-navigation__item main-navigation__item--active">All movies</a>
-    <a href="#watchlist" class="main-navigation__item">Watchlist <span class="main-navigation__item-count">13</span></a>
-    <a href="#history" class="main-navigation__item">History <span class="main-navigation__item-count">4</span></a>
-    <a href="#favorites" class="main-navigation__item">Favorites <span class="main-navigation__item-count">8</span></a>
+    <a href="#watchlist" class="main-navigation__item">Watchlist <span class="main-navigation__item-count">${cardsToFilterMap.watchlist(cards)}</span></a>
+    <a href="#history" class="main-navigation__item">History <span class="main-navigation__item-count">${cardsToFilterMap.history(cards)}</span></a>
+    <a href="#favorites" class="main-navigation__item">Favorites <span class="main-navigation__item-count">${cardsToFilterMap.favorites(cards)}</span></a>
   </div>
   <a href="#stats" class="main-navigation__additional">Stats</a>
 </nav>`);
@@ -14,4 +21,4 @@ export const createSortMenuTemplate = () => (`<ul class="sort">
   <li><a href="#" class="sort__button">Sort by rating</a></li>
 </ul>`);
 
-export const createStatsTemplate = () => '<p>130 291 movies inside</p>';
+export const createStatsTemplate = (cards) => `<p>${cardsToFilterMap.all(cards)} movies inside</p>`;
